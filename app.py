0122f538eaa712ac4155fb0adc55648f7b8d307d
Brian Yoon <byoonie@gmail.com>
2025-12-01 06:01:36 -0800

    Allow connection from non-local devices

--- app.py
+++ app.py
@@ -123,4 +123,4 @@ if __name__ == "__main__":
         db.create_all()
 
     # debug=True auto reloads on code changes.
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
